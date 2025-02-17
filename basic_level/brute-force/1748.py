bcec42d51797e3ba01e49af4373b35cafefd1ff2
literallyme1 <kde7891@naver.com>
2025-02-17 22:11:00 +0900

    2-17-first

---
+++ basic_level/brute-force/1748.py
@@ -0,0 +1,28 @@
+
+def count_digits(N):
+    #총 자리개수 
+    digit_count = 0
+    # 확인할 자릿수 (1, 10, 100 ...)
+    digit_position = 1
+    #자릿수의 개수 
+    num_length = 1
+
+    #whole 값이 있는 지 확인해야 함. 
+    while digit_position * 10 <= N:
+        # n 안에 몇 개의 자릿수 개수가 있는 지 파악
+        count_in_this_range = digit_position * 9
+        #개수 계산 
+        digit_count += count_in_this_range * num_length
+        #자릿수 증가 
+        digit_position *= 10
+        #자릿수 개수 증가
+        num_length += 1 
+    
+    # 🔥 마지막 남은 숫자 처리 (N이 속한 자리수)
+    digit_count += (N - digit_position + 1) * num_length
+
+    print(digit_count)
+
+#입력 
+N = int(input())
+count_digits(N)
---
+++ basic_level/brute-force/6064.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+def find_k(M, N, x, y):
+    #최소공배수 계산
+    LCM = (M*N) // math.gcd(M, N)
+
+    #최소공배수가 넘어가면 주기가 반복됨. 
+    while x <= LCM:
+        if (x-1) % N + 1 == y: #x가 현재년도라고 가정 
+            return x
+        
+        x += M # M 이 될 때 x는 같은 수가 되므로 
+    return -1
+
+T = int(sys.stdin.readline().strip())
+results = []
+
+for _ in range(T):
+    M, N, x, y = map(int, sys.stdin.readline().split())
+    results.append(find_k(M, N, x, y))
+
+#출력
+sys.stdout.write("\n".join(map(str, results)) + "\n")
