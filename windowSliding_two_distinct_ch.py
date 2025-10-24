a1ce75795ae8ce6495f4ea2ebb3646726967c47f
literallyme1 <kde7891@naver.com>
2025-10-24 17:23:56 +0900

    window-sliding

--- windowSliding_two_distinct_ch.py
+++ windowSliding_two_distinct_ch.py
@@ -18,32 +18,6 @@ for right, ch in enumerate(s):
         if dic[s[left]] == 0:
             del dic[s[left]]
         left += 1
-    result = max(result, right - left + 1)
+    result = max(result, right - left + 1) #길이
 
 print(result)
-
-
-
-
-    
-
-
-
-# ## ------------------ 고정 크기 ------------------
-
-# 정수 배열 a = [1, 12, -5, -6, 50, 3]와 정수 k = 4가 주어진다.
-# 길이가 k인 연속 부분배열의 최댓값 합을 구하라.
-
-# a = [1, 12, -5, -6, 50, 3]
-# k = 4
-
-# cur = sum( a[:4])
-# best = cur 
-
-# for i in range(4, len(a)):
-#     cur += a[i]
-#     cur -= a[i - k]
-
-#     best  = max(cur, best)
-    
-# print(best)
