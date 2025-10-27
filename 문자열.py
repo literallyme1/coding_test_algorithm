7e25b55be295c3b8453afc7ad8cc1735009ec2b3
literallyme1 <kde7891@naver.com>
2025-10-27 22:33:08 +0900

    .

---
+++ 문자열.py
@@ -0,0 +1,13 @@
+
+a = [1, 12, -5, -6, 50, 3]
+k = 4
+sum = sum(a[:4])
+result = sum
+left = 0
+for right in range(4, len(a)):
+    sum += a[right]
+    sum -= a[left]
+    left += 1
+    result = max(result, sum)
+
+print(result)
