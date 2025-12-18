751e4a61def2943c8bd20b1800d32101e0933437
literallyme1 <kde7891@naver.com>
2025-12-18 15:21:24 +0900

    bfs

--- BFS/DFS/basic_BFS.py
+++ BFS/DFS/basic_BFS.py
@@ -2,12 +2,17 @@
 from collections import deque
 import sys
 
+def init_graph(n, m):
 
-input = sys.stdin.readline
-# 노드 수, 간선 수, 시작 노드 번호 
-N, M, V = map(int, input().split())
-visited_bfs = [False] * (N + 1) # 1부터 시작 
-graph = [[] for _ in range(N + 1)]
+    graph = [[] for _ in range(N + 1)]
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    for i in range(1, N + 1):
+        graph[i].sort()
+    return graph
 
 def bfs(start_node):
     queue = deque([start_node])
@@ -18,7 +23,18 @@ def bfs(start_node):
         print(current_node, end= ' ')
 
         for next_node in graph[current_node]:
-            
+            #방문 하지 않은 곳, 줄 세우기 
             if not visited_bfs[next_node]:
                 visited_bfs[next_node] = True
                 queue.append(next_node)
+
+
+input = sys.stdin.readline
+# 노드 수, 간선 수, 시작 노드 번호 
+N, M, V = map(int, input().split())
+visited_bfs = [False] * (N + 1) # 1부터 시작 
+
+graph = init_graph(N, M)
+bfs(V)
+
+
