120b06b95dd6c01e2b614c68b37f9bef6acf6aa4
literallyme1 <kde7891@naver.com>
2025-12-19 10:26:00 +0900

    design-parking-fee-day7

---
+++ Implementation/2024_kako_parking_day7.py
@@ -0,0 +1,41 @@
+"""
+Docstring for Implementation.2024_kako_parking_day7
+내 의사결정 코드 -> 기획서 같음. 구체적으로. 
+
+##0. input 관리 (split, 시각, 차량번호(길이 4), 내역(in, out)), HH:MM 형식 시간 ->  분단위 숫자 변환 
+
+## 1. 입출 리스트, 차량 요금 딕셔너리 (누적 주차 시간 -> in, out 반복 가능 딕셔너리 합)
+
+## 2. 입출 리스트에 남아있음. -> 출차 : 23:59 -> 정보 습득 
+
+## 3. 계산 (주차 시간 <= 기본 시간 -> 기본 요금 , 기본 요금 + (주차시간 - 기본 시간) / 단위 시간 * 단위 요금)
+## 4. 나누어 안떨어지면 ceil()
+## 5. 작은 순 리스트 반환 
+"""
+
+"""
+1. [State] 자료구조 & 변수 정의 
+- InMap : <차, 입차시간> -> Map (Hash)가 더 빠름. 
+- AccMap : <차, 누적분> (누적 필수
+- `fee`: int[] fees
+)
+2. [Input] 전처리 (파싱, 정렬) 
+- getTime 함수 : HH*60  + MM로 변환 
+- records 배열 순회하며 공백 Split.
+
+3. [Logic] 핵심 알고리즘 흐름
+- 정보 모으기 루프 
+- IF IN : inMap.put(번호, 시각)
+- ELSE OUT : 
+    inTime = inMap.remove(번호)
+    accTimemap = `(toMin(시각) - inTime)` 더하기
+- 남은 차 처리 
+    - `inMap`에 남은 키들 순회
+  - `(23:59 - inTime)` 계산해서 `accTimeMap`에 추가
+
+4. [Output] 결과 반환 형식
+- carList`: `accTimeMap.keySet()` 꺼내서 **오름차순 정렬**
+- 요금 계산 
+- 시간 <= 기본: `기본요금`
+  - 시간 > 기본: `기본 + Math.ceil((시간-기본)/단위) * 단위요금` (double 이어야 ceil 가능)
+"""
