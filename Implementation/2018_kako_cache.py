f68acc6c794ae23f4de93eb35e820d980179ef95
literallyme1 <kde7891@naver.com>
2026-01-06 14:05:26 +0900

    cache-coding

--- Implementation/2018_kako_cache.py
+++ Implementation/2018_kako_cache.py
@@ -11,4 +11,37 @@
 
 #[Output]도시이름 배열을 순서대로 처리할 때, "총 실행시간"을 출력  
 
+from collections import deque
+def solution(size, cities):
+    total_count = 0
+    cache_list = deque(maxlen=size)
 
+    for city in cities:
+        if city in cache_list:
+            cache_list.remove(city)
+            cache_list.append(city)
+            total_count += 1
+            continue
+    
+        #다 찼으면 
+        if len(cache_list) == size:
+            cache_list.popleft()
+        total_count += 5
+        cache_list.append(city)
+    return total_count
+
+            
+
+
+
+
+def input_normalization(cities):
+    for city in cities:
+        city = city.lower()
+    return cities
+
+
+cities = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]
+cacheSize = 3
+cities = input_normalization(cities)
+print(solution(cacheSize, cities))
