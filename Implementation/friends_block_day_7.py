0ce9df83b85847f91f269ff8f24d38b906265b8f
literallyme1 <kde7891@naver.com>
2026-01-10 14:12:33 +0900

    friends-4block-day7-coding

--- Implementation/friends_block_day_7.py
+++ Implementation/friends_block_day_7.py
@@ -22,3 +22,40 @@
 # -> 반복 while 문 과 조건 
 # ex_block_count = 0 
 #[Output] return ex_block_count
+
+def solution(m, n, board): #m 이 row, 높이
+    # 전처리 
+    board = [ list(string) for string in board]
+    total_count = 0
+    
+    while True:
+        same_set = set()
+        #1. 탐색 
+        for i in range(m -1):
+            for j in range(n -1):
+                if board[i][j] != 0 and board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]: 
+                    same_set.update([(i, j), (i + 1,j), (i,j + 1), (i + 1,j + 1)]) #튜플 넣는 사용법 틀림 
+        
+        #1-1 : 종료조건 
+        if len(same_set) == 0:
+            break
+
+        #2. Same Count 
+        total_count += len(same_set)
+
+        #3. 0으로 지우기 
+        for (i, j) in same_set:
+            board[i][j] = 0
+
+        #4. 중력구현 
+        for col in range(n):
+            col_list = [board[i][col] for i in range(m) if board[i][col] != 0]
+            new_list = [0] * (m - len(col_list)) + col_list # 높이를 알려면 
+            # [0 * (m - len(col_list))] + col_list
+            for i in range(m-1):
+                board[i][col] = new_list[i]
+    return total_count
+        
+    
+         
+print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))
