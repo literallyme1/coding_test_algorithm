47fa55c3a09fca814f13e7750a8cb213d439dda4
literallyme1 <kde7891@naver.com>
2026-01-11 20:57:31 +0900

    matrix-answer-updated

--- Implementation/matrix_outline.py
+++ Implementation/matrix_outline.py
@@ -6,37 +6,45 @@
 #  내부는 회전 X
 
 # graph = [ [j] for j in range(1, rows + 1) for i in range(1, columns+ 1)] #이중이면 뭐가 먼저인지 햇갈
+
+from collections import deque
 def solution(rows, columns, queries):
     #1. 그래프 만들기
     graph = [[ (row * columns) + (j + 1) for j in range(columns)] for row in range(rows)]
     min_list = []
     
     #2. window 로 가져옴. (세부적 다시)
-    for querie in queries:
-        x1, y1, x2, y2 = querie
+    for query in queries:
+        x1, y1, x2, y2 = [i - 1 for i in query]
         square_list = []
         #윗가로
-        for i in range(y1-1, y2-1):
-            # square_list.graph[x1][i]
-            # print(graph[x1][i])
-            print(x1, i)
+        for i in range(y1, y2 + 1):
+            square_list.append((x1, i))
         #오른쪽 높이 
-        for i in range(x1, x2 - 1):
-            # square_list.graph[i][y2]
-            # print(graph[i][y2])
-            print(i, y2)
+        for i in range(x1 + 1, x2 + 1):
+            square_list.append((i, y2))
         #아랫가로
-        for i in range(y2 - 2, y1 - 1, -1):
-            # square_list.graph[x2][i]
-            # print(graph[x2][i])
-            print(x2, i)
+        for i in range(y2 - 1, y1 - 1, -1):
+            square_list.append((x2, i))
         #왼쪽 높이 
-        for i in range(x2-2, x1-2):
-            # square_list.graph[i][y1]
-            # print(graph[i][y1])
-            print(i, y2)
+        for i in range(x2 - 1, x1, -1):
+            square_list.append((i, y1))
         
-    # #3. 회전 
+        #3. 최솟값 넣기 
+        (r, c) = min(square_list)
+        min_list.append(graph[r][c])
+        #4. 회전 
+        rotation = deque(square_list)
+        rotation.rotate()
+
+        #5. 넣어주기 -> import copy deecopy 도 가능  
+        new_graph = [row[:] for row in graph]
+        for (r, c) in square_list:
+            n_r, n_c = rotation.popleft()
+            new_graph[n_r][n_c] = graph[r][c]
+        graph = new_graph
+    return min_list
+
     # last = square_list[-1].remove()
     # square_list.insert(0, last)
 
