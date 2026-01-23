24a0d599e4a50b69d421af58dfa045a562e0b373
literallyme1 <kde7891@naver.com>
2026-01-23 11:44:30 +0900

    mountian-tiling-day7-coding

--- DP/mountain_tiling_day7.py
+++ DP/mountain_tiling_day7.py
@@ -29,3 +29,22 @@
     #intrude
     #not intrude %
 #[Output]
+
+
+def solution(n, tops):
+    dis_num = 10007
+    intrude, not_intrude = 0, 1
+    for i in range(n):
+        prev_not, prev_in = not_intrude, intrude
+        if tops[i] == 1: 
+            not_intrude = (3 * prev_not + 2 * prev_in) % dis_num 
+            intrude = (prev_not + prev_in) % dis_num
+        else :
+            not_intrude = (2 * prev_not + prev_in) % dis_num
+            intrude = (prev_not + prev_in) % dis_num
+    
+    return (not_intrude + intrude) % dis_num
+
+
+
+print(solution(4,[1, 1, 0, 1]))
