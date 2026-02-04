30bd3c71ad9935501eb6ff97f76fb32a8af90aab
literallyme1 <kde7891@naver.com>
2026-02-04 13:10:02 +0900

    heap-more-spicy-day1-design

--- greedy/life_safe_boat_day30.py
+++ greedy/life_safe_boat_day30.py
@@ -1,11 +1,11 @@
 #[State] two_pointer
 #[Input] people	limit	return
-#[Logic] 최대 2명, 무게 제한 0
-#구명보트를 최대한 적게 사용하여 모든 사람을 구출
+#[Logic] 최대 2명, 무게 제한 0 sort
+#구명보트를 최대한 적게 사용하여 모든 사람을 구출 
 # 모두 구출, 가장 큰 사람에 작은사람이 탈 수 있는 지 확인 
 #total, left, right = len(people) - 1 
 # while left <= right:
     #반복 
 
-#[Output]
+#[Output] total 
 
