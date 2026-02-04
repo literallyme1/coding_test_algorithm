1b2bcc43d35e80ed5a8c51b6a2f63c12e34009fd
literallyme1 <kde7891@naver.com>
2026-02-05 05:16:52 +0900

    kakao-more-spicy-code-day1

--- Implementation/kako_heap_more_spicy.py
+++ Implementation/kako_heap_more_spicy.py
@@ -1,9 +1,16 @@
 #[State] heap 
-#[Input] scoville	K	return
+import heapq
+#[Input] scoville	K	
+def solution(scovile, k):
+    heapq.heapify(scovile)
+    total_mix = 0
+    while scovile[0] < k and len(scovile) >= 2:
 #[Logic] 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-#total = 0 
-# while  heap[0] < k and len(heap) >= 2 :
-# pop + pop * 2 -> k 에 도달하는 지 if 문 -> 안되면 
+        last_scoville = heapq.heappop(scovile) + (heapq.heappop(scovile) * 2)
+        heapq.heappush(scovile, last_scoville)
+        total_mix += 1
+
+    return total_mix if len(scovile) > 0 and scovile[0] >= k else -1
 #[pop 하기 전에 ]if len(heap) >= 1 인지 확인 하고 될 때까지  pop 조건 만족 x -1 
 #total +=1 
 #[Output] 섞어야 하는 횟수 return 
