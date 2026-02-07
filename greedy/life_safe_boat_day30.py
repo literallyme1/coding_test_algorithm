c65df97a61aa39d33ec657f62cc7c83767a68cf0
literallyme1 <kde7891@naver.com>
2026-02-07 11:36:55 +0900

    life-boat-day30-design-coding

--- greedy/life_safe_boat_day30.py
+++ greedy/life_safe_boat_day30.py
@@ -1,5 +1,20 @@
 #[State] two_pointer
 #[Input] people	limit	return
+def solution(people, limit):
+    n = len(people)
+    people.sort()
+    right = n -1 
+    left = 0 
+    total = 0 
+    while left <= right : 
+        if people[left] + people[right] <= limit:
+            left += 1 
+        right -= 1
+        total += 1
+    return total
+
+print(solution([70, 50, 80, 50],	100))
+
 #[Logic] 최대 2명, 무게 제한 0 sort
 #구명보트를 최대한 적게 사용하여 모든 사람을 구출 
 # 모두 구출, 가장 큰 사람에 작은사람이 탈 수 있는 지 확인 
