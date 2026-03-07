b09f54e3434ec651f7a42420b296f834e076335c
literallyme1 <kde7891@naver.com>
2026-03-07 19:19:04 +0900

    bigger-number-day2

--- greedy/bigger_number.py
+++ greedy/bigger_number.py
@@ -5,23 +5,6 @@
 #[Output] 가장 큰수 return 
 
 
-#[wrong_understand]
-# def solution(number, k):
-#     n = len(number)
-#     window = n - k
-#     loop_num = n - (window - 1)
-
-#     max = 0
-#     for idx in range(0, loop_num):
-#         start = idx
-#         end = idx + window
-#         if int(number[start: end]) > max:
-#             max = int(number[start: end])
-#     return str(max)
-
-# print(solution("1924",	2))
-
-
 def solution(number, k):
     n = len(number)
     left = n - k
