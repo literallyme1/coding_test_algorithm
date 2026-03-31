c74d39a35dd6e307b835c23e9b116c7ee3e95961
literallyme1 <kde7891@naver.com>
2026-03-31 12:25:09 +0900

    right-answr

--- BFS_DFS/target_number.py
+++ BFS_DFS/target_number.py
@@ -7,17 +7,24 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-def dfs(i, bool, numbers):
-    if i == n - 1:
-        return  numbers[i]
-    
-    dfs(i + 1,True, numbers)
-    dfs(i + 1,False, numbers)
+#재귀 사고
+#1. 인자 (위에서 답, 아래서 답) -> 전해줘야 할 게 달라짐. 
+#2. 탈출 조건 마지막에 원하는 값 (개수, 값, bool)
+#3. 식 : 현재 위치에서 가능한 선택지 + 마지막에 가져온 요소를 어떻게 합할지 
     
 
     
 
 def solution(numbers, target):
+    n = len(numbers)
+    def dfs(i, sum):
+        if i == n:
+            if numbers[i] == target:#배열 범위에 벗어나면
+                return 1
+            else:
+                return 0
+        return dfs(i + 1, sum + numbers[i]) +  dfs(i + 1, sum - numbers[i])
+            
     count = 0
     n = len(numbers)
     
