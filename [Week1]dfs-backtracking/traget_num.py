818743bd04436eb9bbf5826536e2bdc89ab8d2ca
literallyme1 <kde7891@naver.com>
2026-06-09 17:08:21 +0900

    target-num-coding

--- [Week1]dfs-backtracking/traget_num.py
+++ [Week1]dfs-backtracking/traget_num.py
@@ -6,9 +6,36 @@
 #[output] 경우의 수 
 
 
+
+def dfs(result, i):
+    if i + 1 == len(numbers):
+        if result == target:
+            ans += 1
+            return
+    return dfs(result + numbers[i], i + 1), dfs(result - numbers[i], i + 1)
+
+
+    
+    
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+dfs(0, 0)
+
+
+
+
+
+
+
+
+
+
+
 # dfs 는 언제 됐지 ?
 #1. n 이 20 이하, -> 크다 bfs, 이전 -> 다음 영향 DP, 값이 무지막지하게 크다 Greedy
-
 # 조정 
 #1. index 만 넘기면 됨. 
 #2. 함수 -> 전역, 넘길 거 제대로 파악 
+
+
