50db3c078a683c5d998ca7755a8dc8854e9feebe
literallyme1 <kde7891@naver.com>
2026-06-12 10:20:44 +0900

    word-dict

--- [Week1]dfs-backtracking/word_dict.py
+++ [Week1]dfs-backtracking/word_dict.py
@@ -1,6 +1,33 @@
 #[state]
 #[input] matching word
+target = "AAAAE" 
+vowel = ['A', 'E', 'I', 'O', 'U']
 #[logic]
+
+count = 0
+ans = 0 
+def dfs(word, target):
+    global ans, count 
+    if len(word) >  5:
+        return 
+
+    if word :
+        count += 1
+        if word == target:
+            ans = count
+            return 
+    
+    for i in range(5):
+        dfs(word + vowel[i], target)
+
+
+
+    for i in range(5):
+        if not visited[i]:
+            dfs(word + vowel[i], target, count + 1)
+
+dfs("", target, 0)
 #[output] 몇번 째 단어
 
 #  'A', 'E', 'I', 'O', 'U'
+
