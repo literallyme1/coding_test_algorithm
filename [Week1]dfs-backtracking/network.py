9723afb5d2fa23da5baa2b77a1ace37cd9cc03d4
literallyme1 <kde7891@naver.com>
2026-06-17 12:30:19 +0900

    network-day7

--- [Week1]dfs-backtracking/network.py
+++ [Week1]dfs-backtracking/network.py
@@ -7,6 +7,40 @@
 
 n = 3
 computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+count = 0 
+
+visited = [False] * n
+
+def dfs(i):
+    visited[i] = True
+
+    for j in range(n):
+        if computers[i][j] and not visited[j]:
+            dfs(j)
+
+
+for i in range(n):
+    if not visited:
+        dfs(i)
+        count += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 visited = [False] * n 
 def dfs(current):
