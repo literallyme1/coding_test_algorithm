bb5d6c527889d46912947c7f0c44a4df84688baa
literallyme1 <kde7891@naver.com>
2026-07-08 11:01:59 +0900

    dfs-backtracking

---
+++ [Week1]dfs-bfs/dungeons2.py
@@ -0,0 +1,7 @@
+#[state] 백트래킹 dfs
+#[input]
+#[logic]
+#[output]
+
+# bfs 가 아니라 dfs 인 이유
+#- bfs: 최단, 최소 횟수 dfs : 모든 걸 다 시험해보는 상황. 
---
+++ [Week1]dfs-bfs/target_num2.py
@@ -0,0 +1,5 @@
+#[state] 백트래킹 x, dfs (순서가 정해져있으니까)
+#[input] numbers, target
+#[logic]
+#[output] 타겟 넘버를 만드는 방법의 수 각각 dfs
+# len 길이만 보내고 끝까지. 보고 결정. 
